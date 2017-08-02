783cc1fc504d30d1c4cd9d9ffac812619fcaccbd
liyazhou <1286041054@qq.com>
2017-08-02 10:43:48 +0800

    Initial Commit

---
+++ UIWindowTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIWindowTest
+//
+//  Created by liyazhou on 2017/8/2.
+//  Copyright © 2017年 liyazhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIWindowTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIWindowTest
+//
+//  Created by liyazhou on 2017/8/2.
+//  Copyright © 2017年 liyazhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
